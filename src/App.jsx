19c2ff9c1a3e46c12fe23788631305a4ac634abb
unknown <issamaitouahmane@gmail.com>
2022-01-31 18:35:09 +0100

    updated the scrollable target and converted it into a table filled with dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="flex min-h-screen text-gray-700 bg-gray-50">
       {/* sidebar  */}
-      <div className="w-full max-w-[250px] bg-blue-700">
+      <div className="w-full max-w-[250px] bg-blue-600">
         <div className="h-20 bg-white"></div>
       </div>
       {/* header and content */}
@@ -18,12 +18,31 @@ function App() {
         <div className="h-20 w-full bg-white shadow"></div>
         <div className="h-32 bg-white shadow m-3"></div>
         {/* content  */}
-        <main className="p-6 overflow-hidden mb-[800px]">
+        <main className="px-6 py-24 overflow-hidden mx-auto max-w-7xl bg-white rounded shadow">
           <div
             ref={handleContainerRef}
-            className="bg-white rounded overflow-auto"
+            className="overflow-auto"
           >
-            <div className="h-[1000px] w-[2000px] bg-gradient-to-r from-blue-700 via-blue-500 to-blue-300"></div>
+            <table className="">
+              <thead>
+                {Array(10)
+                  .fill("col head")
+                  .map((h, i) => (
+                    <th key={`${h} ${i + 1}`} className="p-4 min-w-[150px] text-left bg-gray-200 border-r border-white">{`${h} ${i + 1}`}</th>
+                  ))}
+              </thead>
+              {Array(30)
+                .fill("col head")
+                .map((r, i) => (
+                  <tr key={`${r} ${i + 1}`} className="border-b">
+                    {Array(10)
+                      .fill("col cell")
+                      .map((c, j) => (
+                        <td key={`${c + i} ${j + 1}`} className="p-4">{`${c} ${j + 1}`}</td>
+                      ))}
+                  </tr>
+                ))}
+            </table>
           </div>
           <StickyScrollbar overflowedTarget={overflowedTarget} />
         </main>
